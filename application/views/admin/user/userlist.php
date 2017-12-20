32316422349da6f43ae2ca8da42de5698e402276
pearlpandz <pearlpandzz@gmail.com>
2017-12-20 07:08:55 +0530

    userlist ui updated - Muthupandi V

--- application/views/admin/user/userlist.php
+++ application/views/admin/user/userlist.php
@@ -1,16 +1,28 @@
-<?php
-    if($this->session->flashdata('item')) {
-        $message = $this->session->flashdata('item');
-    }
-?>
-<?php 
-if( isset($message['message']) ) {
-?>
-<div class="<?php echo $message['class']; ?>">
-    <?php echo $message['message']; ?>
-</div>
-<?php 
-}
-?>
+<!-- stylesheet included -->
+<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/admim_userlist.css">
 
-hiiii
+<div class="userlist_table">
+	<h4>user list</h4>
+	<ul id="userlist"></ul>
+</div>
+<script type="text/javascript">
+var config = {
+    apiKey: "fcafcbc7f2a2ace18ce6721a5bee2a24dc1dad8b",
+    authDomain: "https://createcv-1e398.firebaseio.com/",
+    databaseURL: "https://createcv-1e398.firebaseio.com/",
+    projectId: "createcv-1e398; ?>",
+    storageBucket: "gs://createcv-1e398.appspot.com",
+};
+firebase.initializeApp(config);
+var dbRef = firebase.database();
+var contactsRef = dbRef.ref('users/'); //declared firebase refernce node
+// get values from firebase
+contactsRef.on('value', function(snap) {
+	var i=0;
+	$("#userlist").html('');
+    snap.forEach(function(element) {
+    	var url = '<?php echo base_url() ?>profile/'+element.key;
+    	$("#userlist").append('<li><a href='+url+'><span>'+ parseInt(i=i+1) + '</span><span>' + element.key + '</span></a></li>');
+    });   
+});
+</script>
